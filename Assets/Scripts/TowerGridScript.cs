e41e5dcefcd008a6b1de7eaf7b12d2aecd695af4
GOLDER303 <szymonstepien@interia.pl>
2023-06-10 16:19:58 +0200

    Add the ability to swap sprites of two tiles

--- Assets/Scripts/TowerGridScript.cs
+++ Assets/Scripts/TowerGridScript.cs
@@ -12,6 +12,9 @@ public class TowerGridScript : MonoBehaviour
     private float cellSize = 3f;
     private TowerGridTile[,] gridArray;
 
+    private Vector2Int startDrag = new Vector2Int(-1, -1);
+    private Vector2Int endDrag = new Vector2Int(-1, -1);
+
     public TowerGridScript()
     {
         this.gridArray = new TowerGridTile[gridWidth, gridHeight];
@@ -61,8 +64,73 @@ public class TowerGridScript : MonoBehaviour
         gridArray[towerXPos, towerYPos].SetIsOccupied(true);
     }
 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mouseWorldPosition = GetMouseWorldPosition();
+            startDrag = GetTileFromWorldPosition(mouseWorldPosition);
+            Debug.Log("Start drag: " + startDrag.ToString());
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 mouseWorldPosition = GetMouseWorldPosition();
+            endDrag = GetTileFromWorldPosition(mouseWorldPosition);
+            Debug.Log("End drag: " + endDrag.ToString());
+
+            if (startDrag.x == endDrag.x && startDrag.y == endDrag.y)
+            {
+                return;
+            }
+
+            MoveTower(startDrag, endDrag);
+        }
+    }
+
+    private void MoveTower(Vector2Int startDrag, Vector2Int endDrag)
+    {
+        if (!IsValidTilePosition(startDrag) || !IsValidTilePosition(endDrag))
+        {
+            return;
+        }
+
+        TowerGridTile movedTile = gridArray[startDrag.x, startDrag.y];
+        TowerGridTile destinationTile = gridArray[endDrag.x, endDrag.y];
+
+        Sprite destinationTileSprite = destinationTile.GetTowerTileGameObject().GetComponent<SpriteRenderer>().sprite;
+        destinationTile.GetTowerTileGameObject().GetComponent<SpriteRenderer>().sprite = movedTile.GetTowerTileGameObject().GetComponent<SpriteRenderer>().sprite;
+        movedTile.GetTowerTileGameObject().GetComponent<SpriteRenderer>().sprite = destinationTileSprite;
+    }
+
+    private bool IsValidTilePosition(Vector2Int position)
+    {
+        if (position.x < 0 || position.y < 0 || position.x >= gridWidth || position.y >= gridHeight)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Camera.main.nearClipPlane;
+        Vector3 mouseWorldPosition = Camera.main.ScreenToWorldPoint(mousePos);
+
+        return mouseWorldPosition;
+    }
+
     private Vector3 GetWorldPosition(int x, int y)
     {
         return new Vector3(x, y) * cellSize;
+    }
+
+    private Vector2Int GetTileFromWorldPosition(Vector3 worldPosition)
+    {
+        int x = Mathf.FloorToInt((worldPosition).x / cellSize);
+        int y = Mathf.FloorToInt((worldPosition).y / cellSize);
+
+        return new Vector2Int(x, y);
     }
 }
