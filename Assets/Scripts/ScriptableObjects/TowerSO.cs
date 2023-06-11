856482386b33a57daad942bc81379541e36140f3
GOLDER303 <szymonstepien@interia.pl>
2023-06-11 13:52:10 +0200

    Add states damage and rango to TowerSO

--- Assets/Scripts/ScriptableObjects/TowerSO.cs
+++ Assets/Scripts/ScriptableObjects/TowerSO.cs
@@ -7,4 +7,6 @@ public class TowerSO : ScriptableObject
 {
     public TowerTypeEnum towerType;
     public List<Sprite> statesSprites;
+    public List<float> statesDamage;
+    public List<float> statesRange;
 }
