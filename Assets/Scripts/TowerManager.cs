c54161f0e2288f90b331f7886509b72c1b185a54
GOLDER303 <szymonstepien@interia.pl>
2023-06-11 19:57:02 +0200

    Make tower manager aware of where the enemies are

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class TowerManager : MonoBehaviour
@@ -10,9 +11,58 @@ public class TowerManager : MonoBehaviour
     private int currentState;
     private int numberOfStates;
 
+    private List<Enemy> enemiesInRange = new List<Enemy>();
+    private Enemy currentTarget;
+
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        GetTarget();
+        ShootTarget();
+    }
+
+    private void GetTarget()
+    {
+        if (enemiesInRange.Count > 0)
+        {
+            currentTarget = enemiesInRange[0];
+            return;
+        }
+
+        currentTarget = null;
+    }
+
+    private void ShootTarget()
+    {
+        if (currentTarget == null)
+        {
+            return;
+        }
+
+        Debug.DrawLine(transform.position, currentTarget.transform.position, Color.red);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            enemiesInRange.Add(other.GetComponent<Enemy>());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            if (enemiesInRange.Contains(other.GetComponent<Enemy>()))
+            {
+                enemiesInRange.Remove(other.GetComponent<Enemy>());
+            }
+        }
     }
 
     public void SetData(TowerSO towerSO)
